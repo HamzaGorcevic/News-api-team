fa1dacab69c587ec6540cb331a9e4956fbfad74c
HamzaGorcevic <gorcevichamza76@gmail.com>
2022-09-05 17:50:26 +0200

    final stage

--- src/News-api/context.js
+++ src/News-api/context.js
@@ -1,19 +1,14 @@
-import { useState } from "react"
-import { createContext } from "react"
+import { useState } from "react";
+import { createContext } from "react";
 
+const myContext = createContext();
+export { myContext };
+export default function ContextProvide({ children }) {
+  const [sentNews, setSentNews] = useState([]);
 
-
-const myContext = createContext()
-export {myContext}
-export default function ContextProvide({children}){
-    const[sentNews,setSentNews] = useState([])
-
-    return <myContext.Provider value={{sentNews,setSentNews}}>
-        {children}
-        
+  return (
+    <myContext.Provider value={{ sentNews, setSentNews }}>
+      {children}
     </myContext.Provider>
-
-    
-
-
+  );
 }
